c733fe628fd5c26cc3ef331eac7101f659d66398
gidger <stephen.a.m.gidge@gmail.com>
2021-10-14 17:38:48 -0400

    Updating with Seattle Kraken graphics.

--- assets/images/team logos/svgConv.py
+++ assets/images/team logos/svgConv.py
@@ -3,11 +3,14 @@ from os import path
 from cairosvg import svg2png
 
 def main():
+    # Converts all .svg files in the source directory to .png. Saves to the destination directory.
+    # Set source and destination as needed.
+    
     # source = '/home/pi/nhl_scoreboard/Assets/Images/Team Logos/SVG/'
     # destination = '/home/pi/nhl_scoreboard/Assets/Images/Team Logos/PNG/'
 
-    source = '/Users/stephen/Development/nhl_scoreboard/Assets/Images/Team Logos/SVG/'
-    destination = '/Users/stephen/Development/nhl_scoreboard/Assets/Images/Team Logos/PNG/'
+    source = '/Users/stephen/Developer/rpi-led-nhl-scoreboard/assets/images/team logos/svg/'
+    destination = '/Users/stephen/Developer/rpi-led-nhl-scoreboard/assets/images/team logos/png/'
 
     for filename in os.listdir(source):
         print(filename)
