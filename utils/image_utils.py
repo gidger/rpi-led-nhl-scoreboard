b529864d6bdd59fd2db057e8f8ac5f09286c5a1a
gidger <stephen.a.m.gidge@gmail.com>
2025-11-11 19:22:38 -0500

    Clarify comments

--- utils/image_utils.py
+++ utils/image_utils.py
@@ -11,12 +11,12 @@ def crop_image(image):
         image: Cropped image.
     """
 
-    # Get the bounding box of the image. Aka, boundries of what's non-transparent. Crop the image to the contents of the bounding box.
+    # Get the bounding box of the image. Aka, boundaries of what's non-transparent. Crop the image to the contents of the bounding box.
     bbox = image.getbbox()
     image = image.crop(bbox)
     
     # Create a new image object for the output image. Paste the cropped image onto the new image.
-    cropped_image = Image.new('RGB', image.size)
+    cropped_image = Image.new('RGB', image.size) # Note that since we define this as RGB, there will be no transparency in the resulting image.
     cropped_image.paste(image)
 
     return cropped_image
