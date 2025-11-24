610349f41dffc83b25985fa770e43e49068fd928
gidger <stephen.a.m.gidge@gmail.com>
2025-11-24 00:22:09 -0500

    Clean logic when fav team is currently playing

--- scenes/fav_team_next_game_scenes/fav_team_next_game_scene_nhl.py
+++ scenes/fav_team_next_game_scenes/fav_team_next_game_scene_nhl.py
@@ -35,9 +35,10 @@ class NHLFavTeamNextGameScene(FavTeamNextGameScene):
             for team in self.favourite_teams:
                 next_game_details = data.nhl_data.get_next_game(team)
                 
-                # If a game is in progress, and display_if_in_progress is False, exit without displaying.s
+                # If a game is in progress, and display_if_in_progress is False, exit without displaying anything.
                 if next_game_details['is_today'] and next_game_details['has_started'] and not self.settings['display_if_in_progress']:
-                    return
+                    continue # Move on to the next fav team.
+                
                 # Otherwise, build image and display.
                 else:
                     self.build_next_game_image(team, next_game_details)
